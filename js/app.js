da610a47c1d6899639c22ce3175437af38ddb1ee
Anna Kreczmer-Jasiuk <anna.kreczmer-jasiuk@pl.ibm.com>
2018-10-04 09:02:25 +0200

    Player and enemies moves added, success modal

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ Enemy.prototype.update = function(dt) {
      this.speed = this.speed + 10;
      this.x = -100;
 }
-  if ((Math.abs(this.x - player.x)<=50)&&(Math.abs(this.y-player.y)<=20)){
+  if ((Math.abs(this.x - player.x)<=50)&&(Math.abs(this.y-player.y)<=20)){//resets game in case of collision between player and one of enemies
   player.x = 200;
   player.y=450;
   enemy1.speed=150;
@@ -69,22 +69,40 @@ Player.prototype.render = function() {
 };
 
 var direction;
+var modal;
 Player.prototype.handleInput = function(direction) {
-    if(direction==='left'&&this.x>20){
-      this.x-=100;
-    }else if (direction==='right'&&this.x<370){
-      this.x+=100;
-    }else if (direction==='up'&&this.y>0){
-      this.y-=80;
-    }else if (direction==='down'&&this.y<450){
-      this.y+=80;
+
+  if (this.y!==-30){
+          if(direction==='left'&&this.x>20){// moves the player depending what key is pressed
+            this.x-=100;
+          }else if (direction==='right'&&this.x<370){
+            this.x+=100;
+          }else if (direction==='up'&&this.y>0){
+            this.y-=80;
+          }else if (direction==='down'&&this.y<450){
+            this.y+=80;
+          }
+
+          if(this.y===-30){//once player reaches the water, the congratulations modal is displayed, the enemies stop and further movements of the player are not possible
+              modal = document.createElement('div');
+              modal.textContent ='Congratulations! You won!';
+              document.querySelector('body').appendChild(modal);
+              modal.setAttribute('style','position:fixed; z-index:3; top: 25%; left: 25%; width:50%;font-size: 30px; background-color:#A7A69D; padding:10px;');
+              modalOverlay = document.createElement('div');
+              document.querySelector('body').appendChild(modalOverlay);
+              modalOverlay.setAttribute('style','position:fixed; z-index:2; width:100%; height:100%; background-color:#000; opacity:0.5; top: 0; left:0');
+              enemy1.speed=0;
+              enemy2.speed=0;
+              enemy3.speed=0;
+              enemy4.speed=0;
+              enemy5.speed=0;
+            }
     };
 };
 
-
 // Now instantiate your objects.
 var enemy1 = new Enemy (0,50,150);
-var enemy2 = new Enemy (50,130,150);
+var enemy2 = new Enemy (150,130,150);
 var enemy3 = new Enemy (0,210,90);
 var enemy4 = new Enemy (-50,210,180);
 var enemy5 = new Enemy (100,210,180);
